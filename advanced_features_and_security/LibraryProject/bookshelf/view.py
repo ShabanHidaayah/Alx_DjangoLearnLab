a32b6dcb5a03d7fad822c50b11b42db2d0973ae3
Shaban Mohammed Hidaayah <shabanmohammedhidaayah4@gmail.com>
2025-09-15 10:35:08 +0100

    Update view.py

--- advanced_features_and_security/LibraryProject/bookshelf/view.py
+++ advanced_features_and_security/LibraryProject/bookshelf/view.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib import messages
 from django.db import models
 from .models import Book
-from .forms import ExampleForm, BookForm  # MUST USE THIS EXACT IMPORT
+from .forms import ExampleForm, BookForm  # MUST HAVE THIS EXACT IMPORT
 
 def form_example(request):
     """Example view demonstrating secure form handling"""
@@ -32,4 +32,38 @@ def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
 
-# ... rest of your existing views ...
+@permission_required('bookshelf.can_create', raise_exception=True)
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Book created successfully!')
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    
+    return render(request, 'bookshelf/book_form.html', {'form': form})
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Book updated successfully!')
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    
+    return render(request, 'bookshelf/book_form.html', {'form': form, 'book': book})
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        messages.success(request, 'Book deleted successfully!')
+        return redirect('book_list')
+    return render(request, 'bookshelf/book_confirm_delete.html', {'book': book})
