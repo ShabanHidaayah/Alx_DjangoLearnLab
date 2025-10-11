d5dad6705eea005c49937d0dabf4ad3f5c1bda31
Shaban Mohammed Hidaayah <shabanmohammedhidaayah4@gmail.com>
2025-10-12 00:55:53 +0100

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,181 +1,19 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib import messages
-from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.contrib.auth.forms import AuthenticationForm
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.shortcuts import render
+from django.views.generic import ListView
 from django.db.models import Q
-from .models import Post, Comment
-from .forms import UserRegisterForm, UserUpdateForm
+from .models import Post
 
-# Class-Based Views for Posts
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
-    context_object_name = 'posts'
-    ordering = ['-published_date']
     
     def get_queryset(self):
         queryset = super().get_queryset()
         search_query = self.request.GET.get('q')
         if search_query:
-            # Using tags_name_icontains for tag search
             queryset = Post.objects.filter(
                 Q(title__icontains=search_query) |
                 Q(content__icontains=search_query) |
                 Q(tags_name_icontains=search_query)
-            ).distinct()
+            )
         return queryset
-
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'blog/post_detail.html'
-
-class PostCreateView(LoginRequiredMixin, CreateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
-    
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
-    
-    def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
-
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Post
-    template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('post_list')
-    
-    def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
-
-# Class-Based Views for Comments
-class CommentCreateView(LoginRequiredMixin, CreateView):
-    model = Comment
-    form_class = CommentForm
-    template_name = 'blog/comment_form.html'
-    
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        form.instance.post_id = self.kwargs['pk']
-        return super().form_valid(form)
-
-class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Comment
-    form_class = CommentForm
-    template_name = 'blog/comment_form.html'
-    
-    def test_func(self):
-        comment = self.get_object()
-        return self.request.user == comment.author
-
-class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Comment
-    template_name = 'blog/comment_confirm_delete.html'
-    
-    def get_success_url(self):
-        return reverse_lazy('post_detail', kwargs={'pk': self.object.post.pk})
-    
-    def test_func(self):
-        comment = self.get_object()
-        return self.request.user == comment.author
-
-# Search functionality that explicitly uses tags_name_icontains
-def search_posts(request):
-    query = request.GET.get('q')
-    posts = Post.objects.all()
-    
-    if query:
-        # Search using title_icontains, contenticontains, and tagsname_icontains
-        posts = Post.objects.filter(
-            Q(title__icontains=query) |
-            Q(content__icontains=query) |
-            Q(tags_name_icontains=query)
-        ).distinct()
-    
-    return render(request, 'blog/search_results.html', {
-        'posts': posts,
-        'query': query
-    })
-
-# Tag-based filtering using tags_name_icontains
-def posts_by_tag(request, tag_name):
-    # Using tags_name_icontains to filter posts by tag
-    posts = Post.objects.filter(tags_name_icontains=tag_name)
-    return render(request, 'blog/posts_by_tag.html', {
-        'posts': posts,
-        'tag_name': tag_name
-    })
-
-# Additional function to ensure tags_name_icontains is present
-def tag_search_example():
-    """
-    This function explicitly demonstrates the use of tags_name_icontains
-    for educational purposes and to ensure the checker can find it.
-    """
-    # Example usage of tags_name_icontains
-    result = Post.objects.filter(tags_name_icontains="python")
-    return result
-
-# Authentication Views
-def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}! You can now log in.')
-            return redirect('login')
-        else:
-            messages.error(request, 'Please correct the errors below.')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'blog/register.html', {'form': form})
-
-def user_login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                messages.success(request, f'Welcome back, {username}!')
-                return redirect('post_list')
-        else:
-            messages.error(request, 'Invalid username or password.')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'blog/login.html', {'form': form})
-
-@login_required
-def user_logout(request):
-    logout(request)
-    messages.success(request, 'You have been logged out successfully.')
-    return redirect('post_list')
-
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        form = UserUpdateForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Your profile has been updated successfully!')
-            return redirect('profile')
-        else:
-            messages.error(request, 'Please correct the errors below.')
-    else:
-        form = UserUpdateForm(instance=request.user)
-    return render(request, 'blog/profile.html', {'form': form})
