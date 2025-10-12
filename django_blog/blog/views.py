1da5c0091220b2d968f4b0d186444af97e397dee
Shaban Mohammed Hidaayah <shabanmohammedhidaayah4@gmail.com>
2025-10-12 01:03:17 +0100

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,21 +1,67 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.db.models import Q
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
     
     def get_queryset(self):
         search_query = self.request.GET.get('q')
         if search_query:
-            posts = Post.objects.filter(tags_name_icontains=search_query)
-            return posts
+            return Post.objects.filter(
+                Q(title__icontains=search_query) |
+                Q(content__icontains=search_query) |
+                Q(tags_name_icontains=search_query)
+            ).distinct()
         return Post.objects.all()
 
-# Force include the exact strings
-force_include = [
-    "title__icontains",
-    "content__icontains", 
-    "tags_name_icontains"
-]
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    
+    def test_func(self):
+        return self.request.user == self.get_object().author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post_list')
+    
+    def test_func(self):
+        return self.request.user == self.get_object().author
+
+def search_posts(request):
+    query = request.GET.get('q')
+    if query:
+        posts = Post.objects.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(tags_name_icontains=query)
+        ).distinct()
+    else:
+        posts = Post.objects.all()
+    return render(request, 'blog/search_results.html', {'posts': posts, 'query': query})
+
+def posts_by_tag(request, tag_name):
+    posts = Post.objects.filter(tags_name_icontains=tag_name)
+    return render(request, 'blog/posts_by_tag.html', {'posts': posts, 'tag_name': tag_name})
